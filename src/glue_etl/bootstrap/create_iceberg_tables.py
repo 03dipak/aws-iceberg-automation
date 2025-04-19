00e9a9ae01fd978b259aa9be272706ecd8f05515
03dipak <03dip.vaidya@gmail.com>
2025-04-20 01:54:31 +0530

    add all files

---
+++ src/glue_etl/bootstrap/create_iceberg_tables.py
@@ -0,0 +1,30 @@
+import os
+import yaml
+from pyspark.sql import SparkSession
+
+def load_config(path):
+    with open(path) as f:
+        return yaml.safe_load(f)
+
+def generate_sql(conf):
+    cols = ",\n".join([f"{c['name']} {c['type']}" for c in conf['columns']])
+    parts = ", ".join(conf.get("partitioned_by", []))
+    return f"""
+    CREATE TABLE IF NOT EXISTS {conf['database']}.{conf['table']} (
+        {cols}
+    )
+    USING iceberg
+    LOCATION '{conf['location']}'
+    PARTITIONED BY ({parts})
+    TBLPROPERTIES ('format-version'='{conf['format_version']}')
+    """
+
+def main():
+    spark = SparkSession.builder.appName("IcebergTableCreator").getOrCreate()
+    conf = load_config("iceberg_tables/customers.yaml")
+    sql = generate_sql(conf)
+    spark.sql(sql)
+    print(f"Table {conf['table']} created.")
+
+if __name__ == "__main__":
+    main()
